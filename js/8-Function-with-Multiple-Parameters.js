3428d021e3acdd0a3d2f24e8fd1cb0cffeab05d5
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-04-05 10:49:27 +0530

    Function-with-Multiple-Parameters

---
+++ js/8-Function-with-Multiple-Parameters.js
@@ -0,0 +1,17 @@
+// function apples(one , two){
+//     document.write(one + "is better then" + two + "<br>")
+// }
+// apples("fayyaz","rahul")
+// apples("Danish","Mukesh")
+// apples("uzair","Deepak")
+
+
+// function fay1(a,b,c,d) {
+//    document.write(a + " is bad boy " + b + " is good boy " + c +" is bad boy " + d + " is good boy" + "<br>") 
+// }
+// fay1("danish-chouhan","fayyazurrehman","uzair-khilji","ayyazurrehman")
+
+function functionName(a , b){
+    document.write(a + b)
+}
+functionName("fayyaz","khilji")
