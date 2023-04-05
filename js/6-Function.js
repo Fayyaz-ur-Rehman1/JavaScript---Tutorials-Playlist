dd70d7d913f0c980ee7502c464110120a9632754
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-04-05 08:39:38 +0530

    Function

---
+++ js/6-Function.js
@@ -0,0 +1,12 @@
+ function fn() {
+    alert("oye!!")
+}
+// fn()
+
+function fn1() {
+     document.write("my name is fayyazurrehman")
+}
+function fn3(){
+    document.write('my name is fayyaz')
+}
+fn3()
