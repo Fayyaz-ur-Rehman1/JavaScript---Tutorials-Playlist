576ace118b268bda06dbd2e688489bc94757ef29
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-04-05 08:13:03 +0530

    Using-Variables-With-String

---
+++ js/5-Using-Variables-with-String.js
@@ -0,0 +1,13 @@
+var name = 'fayyazurrehman';
+var age = 17;
+document.write('my name is ' + name)
+document.write("<br>")
+document.write('my name is ' ,  name)
+document.write("<br>")
+document.write("my name is " + name + ' my age is ' + age)
+document.write("<br>")
+
+var name = 'fayyazurrehman'
+var fname = 'fayyaz'
+var lname = 'urrehman'
+document.write( name + ' ' +fname + ' ' +lname)
