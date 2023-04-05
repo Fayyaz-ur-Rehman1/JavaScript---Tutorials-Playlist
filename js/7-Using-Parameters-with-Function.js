212926afb431d3a4d87ad16f81a8c369fb5edc69
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-04-05 10:48:02 +0530

    Using-Parameters-with-Function

---
+++ js/7-Using-Parameters-with-Function.js
@@ -0,0 +1,39 @@
+function meatball(x){
+    alert("i love " + x);
+}
+meatball("bacon");
+meatball("natalie protman");
+meatball("good will hunting")
+
+
+
+function fay(v){
+    document.write("name" + v)
+}
+fay("fayyazurrehman <br>")
+fay("uzair khilji <br>")
+fay("danish chouhan <br>")
+fay("ayyazkhilji <br>")
+
+
+function functionName(a , b){
+    document.write(a + b)
+}
+function fn5(c,d){
+    document.write(c * d)
+}
+fn5(4,5)
+document.write("<br>")
+fn5(2,2)
+document.write("<br>")
+fn5(3,3)
+document.write("<br>")
+fn5(1,4)
+document.write("<br>")
+
+functionName("rehman","raheem")
+document.write("<br>")
+functionName("fayyaz","khilji")
+document.write("<br>")
+functionName("khan" , "khilji")  
+document.write("<br>")
