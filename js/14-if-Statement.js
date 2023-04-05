7f0a029d450a7aa47d5f79a72a1ffeedcb37ce66
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-04-06 00:57:33 +0530

    if-Statement

---
+++ js/14-if-Statement.js
@@ -0,0 +1,73 @@
+var  apples = 56;
+var hotdogs = 56;
+
+if(apples ==hotdogs) {
+    document.write("yah it worked")
+}
+
+document.write("<br>")
+
+var  a = 56;
+var b = 6;
+
+if(a == b) {
+    document.write("yah it worked")
+}
+
+document.write("<br>")
+
+
+var  c = 56;
+var d = 6;
+
+if(c > d) {
+    document.write("yah it worked")
+}
+
+document.write("<br>")
+
+
+var  e = 5;
+var f = 6;
+
+if(e < f) {
+    document.write("yah it worked")
+}
+
+document.write("<br>")
+
+
+var  c = 6;
+var d = 6;
+
+if(c >= d) {
+    document.write("yah it worked")
+}
+
+document.write("<br>")
+
+var  c = 6;
+var d = 6;
+
+if(c <= d) {
+    document.write("yah it worked")
+}
+
+document.write("<br>")
+
+
+var  c = 6;
+var d = 0;
+
+if(c != d) {
+    document.write("yah it worked")
+}
+
+document.write("<br>")
+
+var  c = 6;
+var d = 6;
+
+if(c != d) {
+    document.write("yah it worked")
+}
