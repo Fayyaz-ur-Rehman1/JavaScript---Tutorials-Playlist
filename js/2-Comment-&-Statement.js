95e49df9e80245991d294ed3b34fbe4f74c156f8
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-04-05 06:41:53 +0530

    comment-and-statement-in-javascript

---
+++ js/2-Comment-&-Statement.js
@@ -0,0 +1,7 @@
+// document.write("my name is fayyaz") // this is single line comment in javascript //
+/*document.write("i study in class 11th")
+document.write("my school name is molana abul kalam ajad")*/ //this is multiple line comment /**/
+
+document.write("hello guys"); // any place we write samicaloumn ; that is called statement
+document.write("asslamuailkum <br>")
+document.write('walikumasslam'); // this is one satement
