d0e7ccbe62d6a73951c0c17491cafd99963ae998
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-04-06 02:53:24 +0530

    Switch-Case

---
+++ js/18-Switch-case.js
@@ -0,0 +1,65 @@
+var a = "Goal";
+
+switch (a) {
+    case "name":
+        document.write("fayyazurrehman");
+        break;
+
+    case "class":
+        document.write("11th");
+        break;
+
+    case "sub":
+        document.write("Commerce");
+        break;
+
+    case "Age":
+        document.write("17");
+        break;
+
+    case "Goal":
+        document.write("full stacks doveloper");
+        break;
+
+        default : 
+        document.write("value is not here")
+}
+
+
+document.write("<br>")
+
+
+var day = 1
+
+switch (day) {
+    case 0:
+        document.write("Today is Monday")
+        break;
+
+    case 1:
+        document.write("Today is Tuesday")
+        break;
+
+    case 2:
+        document.write("Today is Wednesday")
+        break;
+
+    case 3:
+        document.write("Today is Thrusday")
+        break;
+
+    case 4:
+        document.write("Today is Friday")
+        break;
+
+    case 5:
+        document.write("Today is Satrday")
+        break;
+
+    case 6:
+        document, write("Today is Sunday")
+        break;
+
+    default:
+        document.write("Error")
+}
