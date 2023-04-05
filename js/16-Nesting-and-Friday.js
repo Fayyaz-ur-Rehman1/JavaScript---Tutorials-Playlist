efb88d784459f3561a21dd449732d03a30c367b2
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-04-06 01:58:50 +0530

    Nesting-and-Friday

---
+++ js/16-Nesting-and-Friday.js
@@ -0,0 +1,47 @@
+var firstName = "fayyaz";
+var lastName = "urrehman";
+
+if(firstName==="fayyaz"){
+    if(lastName==="urrehman");
+    document.write("true is my name")
+}else {
+    document.write("false is my name")
+}
+
+document.write("<br>")
+
+var firstName = "fayyaz";
+var lastName = "urrehman";
+
+if(firstName==="fayyz"){
+    if(lastName==="urrehman");
+    document.write("true is my name");
+}else {
+    document.write("false is my name");
+}
+
+document.write("<br>");
+
+var a = 34;
+var b = 45;
+
+if(a != 3){
+    if(b  != 4){
+      document.write("this is true")
+    }
+}else{
+    document.write("this is false")
+}
+
+document.write("<br>")
+
+var a = 34;
+var b = 45;
+
+if(a != 3){
+    if(b  == 45){
+      document.write("this is true")
+    }
+}else{
+    document.write("this is false")
+}
