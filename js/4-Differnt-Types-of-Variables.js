734d2ab81c39ade831a83a99035fe696060fe735
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-04-05 07:57:30 +0530

    Diffrent-Types-of-Variables

---
+++ js/4-Differnt-Types-of-Variables.js
@@ -0,0 +1,31 @@
+var a = "fayyazurrehman";
+document.write(a);
+document.write("<br>");
+
+var b = 0.244334;
+document.write(b);
+document.write("<br>");
+
+var v = -0.3553574;
+document.write(v);
+document.write("<br>");
+
+var w = true;
+document.write(w);
+document.write("<br>");
+
+var t = false;
+document.write(t);
+document.write("<br>");
+
+var f = null;
+document.write(f);
+document.write("<br>")
+
+var y = "i am fayyaz,\"i live in india\"";
+document.write(y)
+document.write("<br>")
+
+
+var z = "\"i am fayyaz urrehman\""
+document.write(z)
