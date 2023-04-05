976e7d0be18a40d758e0697b564865e79f7cf1b4
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-04-05 21:27:34 +0530

    Assigments-Opreaters

---
+++ js/13-Assigment-Opretors.js
@@ -0,0 +1,23 @@
+var a = 24;
+a += 54;
+document.write(a)
+
+document.write("<br>")
+
+var y = 56;
+ y *= 2
+document.write(y)
+
+document.write("<br>")
+
+var j = 7;
+var w = 9;
+var c = j /= w;
+document.write(c)
+
+document.write("<br>")
+
+var h = 98;
+h /= 78
+document.write(h)
+// += , -= , /= , %= , *= this is arthametic opreater
