7002c820a8809cabc03afa38c08c1edb6b664460
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-04-05 10:50:10 +0530

    the-return-Statement

---
+++ js/9-the-return-Statement.js
@@ -0,0 +1,36 @@
+function num(a, b) {
+    var c = a + b
+    return c
+}
+document.write(num(3, 4))
+document.write("<br>")
+
+
+function num1(a, b) {
+    return a * b
+}
+document.write(num1(4, 6))
+document.write("<br>")
+
+
+function tooeasy() {
+    return "my name is fayyazurrehman"
+}
+document.write(tooeasy())
+
+document.write("<br>")
+
+
+function sum(math,english,science){
+    var d = math + english + science
+    return d 
+}
+
+function percentage(tt){
+     var per = tt/30*100
+  document.write(per)
+    
+}
+
+var total = sum(40 , 40 , 40);
+percentage(total)
