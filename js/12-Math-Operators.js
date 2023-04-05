cc7f7ada071a45a817ef80ffcbdd9f7ca43e7c48
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-04-05 21:11:17 +0530

    Math-Operators

---
+++ js/12-Math-Operators.js
@@ -0,0 +1,66 @@
+var a = 4 + 7;
+document.write(a)
+
+document.write("<br>")
+
+var a = 4;
+var b = 5;
+var c = a + b;
+document.write(c)
+
+document.write("<br>")
+
+var d = 3;
+var g = 7;
+var j = d * g;
+document.write(j)
+
+document.write("<br>")
+
+var o = 9;
+var i = 4;
+var u = o - i;
+document.write(u)
+
+document.write("<br>")
+
+var y = 6;
+var t = 10;
+var u = y * t;
+document.write(u)
+
+document.write("<br>")
+
+var p = 8;
+var y = 20;
+var z = p % y;
+document.write(z)
+
+document.write("<br>")
+
+var e = 2;
+var l = 8;
+var o = e/l;
+document.write(o)
+
+document.write("<br>")
+
+var v = 3;
+v++;
+document.write(v)
+
+document.write("<br>")
+
+var u = 24;
+u-- 
+document.write(u)
+
+document.write("<br>")
+
+var u = 87;
+document.write(u++)
+
+document.write("<br>")
+
+var w = 78;
+document.write(w--)
