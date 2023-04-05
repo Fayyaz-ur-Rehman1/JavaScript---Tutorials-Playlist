89becaf2eb4e22c8efbdcbbe00d58abfebc22beb
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-04-06 02:10:36 +0530

    Complex-Condition

---
+++ js/17-Complex-Condition.js
@@ -0,0 +1,31 @@
+var a = 34;
+var b = 30;
+
+if( (a > b) && (a > b) ){
+    document.write("true");
+}else{
+    document.write("false");
+}
+
+document.write("<br>")
+
+var fname = 'fayyaz';
+var lname = 'urrehman';
+
+if( ( fname==='fayyaz') && (lname==='urrehman') ){
+    document.write("true");
+}else{
+    document.write("false");
+}
+
+document.write("<br>")
+
+
+var fname = 'fayyaz';
+var lname = 'urrehman';
+
+if( ( fname==='fayyaz') && (lname==='urrehmn') ){
+    document.write("true");
+}else{
+    document.write("false");
+}
