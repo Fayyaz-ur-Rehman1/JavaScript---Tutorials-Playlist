6f4bbc90d87e0031319ff715c8e309b1706eed1c
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-04-05 07:31:34 +0530

    Variable

---
+++ js/3-Variables.js
@@ -0,0 +1,10 @@
+// Variables
+var x = 23;
+document.write(x);
+
+// var x = "fayyaz";
+// document.write(X);//this is not show output beacuse var name write lowercase
+//  and find the upercase then not run out put beacuse both are differt in variable
+
+var fay_y=a$z = "fayyazurrehman";
+document.write(fay_y=a$z); //this sing are use in variable
