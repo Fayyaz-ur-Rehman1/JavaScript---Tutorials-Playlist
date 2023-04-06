476c072e507818bef10675fbb696b9d5ad94ccbf
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-04-06 10:27:50 +0530

    do-While-Loop

---
+++ js/21-do-While-Loop.js
@@ -0,0 +1,23 @@
+var a = 1;
+
+do{
+   document.write(a + "Hello -People" + "<br>");
+   a++
+}while(a < 10)
+
+var a = 1;
+
+
+do{
+   document.write(a + "Hello" + "<br>");
+   a++
+}while(a > 10)
+
+
+var b = 2;
+
+do{
+    document.write(b + "<br>")
+    b=b+2
+}while(b < 22)
+
