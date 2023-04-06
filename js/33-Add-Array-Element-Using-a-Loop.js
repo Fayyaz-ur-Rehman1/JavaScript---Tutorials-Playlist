884e2a0a59e3d70c8bfc00a1c457126ea5af6581
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-04-07 02:42:49 +0530

    Array-Element-Using-a-Loop

---
+++ js/33-Add-Array-Element-Using-a-Loop.js
@@ -0,0 +1,10 @@
+var pie = prompt("Enter Your Name :" ,"");
+document.write('Hello' + pie);
+
+var arr = new Array(3);
+
+for(i=0; i<3; i++){
+    arr[i] = prompt("Add something to the ARRAY:");
+}
+
+document.write(pie[1] + arr[1] + arr[2])
