1ec509da1e00a9968057811483b1f8de030087ae
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-04-07 02:39:39 +0530

    Array-Other-way-to-cerate-Arrays

---
+++ js/29-Arrays-Ways-to-Create-Arrays.js
@@ -0,0 +1,17 @@
+var arr = new Array(4);
+arr[0] = "fayyazurrehman",
+arr[1] = "saddiqurrehman",
+arr[2] = "danishchouhan",
+arr[3] = "imran";
+document.write(arr.join(" - ") + "<br>");
+
+var arr1 = new Array(2);
+
+var a = 2;
+var b = 3;
+var c = a + b;
+
+arr1[0] = "khan";
+arr1[1] = "shan"
+arr1[2] = "bahijaan"
+document.write(arr1)
