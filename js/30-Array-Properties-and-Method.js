5f62931f3cc9a765f253d53b056d56fc1b170826
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-04-07 02:40:36 +0530

    Array-Propertires-and-Method

---
+++ js/30-Array-Properties-and-Method.js
@@ -0,0 +1,10 @@
+var a = ['my name','is','=>']
+var b = ['fayyaz','ur','rehman']
+var c = a.concat(b)
+document.write(c + "<br>")
+document.write(c[3])
+
+document.write("<br>")
+
+var a = ["hello", "guys", "how", "are u"]
+document.write(a.length)
