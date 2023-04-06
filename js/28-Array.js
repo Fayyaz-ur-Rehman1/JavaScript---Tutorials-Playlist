720a9aabd0deae291fef0e389d77b79942efc816
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-04-07 02:38:31 +0530

    Array

---
+++ js/28-Array.js
@@ -0,0 +1,7 @@
+var arr = new Array("fayyaz","uzair","danish","ayyaz");
+document.write(arr + "<br>");
+document.write(arr[2] + "<br>");
+document.write(arr[1] + " " + arr[3] + "<br>");
+
+var arr1 = ['fayyaz','danish','uzair','ayyaz'];
+document.write(arr1[1])
