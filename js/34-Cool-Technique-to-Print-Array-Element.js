9eb3e92151eab4aa58747f45549e9890460430fa
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-04-07 02:43:46 +0530

    Cool-Technique-to-Print-Array-Element

---
+++ js/34-Cool-Technique-to-Print-Array-Element.js
@@ -0,0 +1,12 @@
+  var stuff = new Array("apples" , 'pears' , 'tuna', 'ham');
+  stuff.sort();
+
+  for(i=0 ; i < stuff.length ; i++){
+    document.write(stuff[i] + "<br>")
+  }
+
+var a = new Array("fayyaz","ayyaz","uzair");
+a.sort();
+for(i=0 ; i<a.length ; i++){
+    document.write(a[i] + "<br>")
+}
