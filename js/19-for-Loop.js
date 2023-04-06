6fa565e0f07234d8afdf85cf03de93c5692287da
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-04-06 10:26:31 +0530

    For-Loop

---
+++ js/19-for-Loop.js
@@ -0,0 +1,17 @@
+ for(x=0 ; x < 10 ; x++){
+   document.write("my name is fayyaz" + "<br>");
+ }
+
+ var a = "fayyazurrehman"
+ for(i = 0 ; i < 10 ; i++){
+    document.write(a + "<br>")
+ }
+
+ 
+ for(let o = 1 ; o <= 100 ; o++){
+    document.write( o +"<br>")
+ }
+
+ for(let o = 3 ; o <= 30 ; o=o+3){
+   document.write( o +"<br>")
+}
