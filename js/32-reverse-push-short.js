1b397930749a22051633a8302648bbf9cc8fd69b
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-04-07 02:42:07 +0530

    reverse-push-sort

---
+++ js/32-reverse-push-short.js
@@ -0,0 +1,20 @@
+var a = new Array("fayyaz","ayyaz","uzair","khilji");
+var b = a.sort()
+document.write(a + "<br>");
+
+var a = new Array("fayyaz","ayyaz","uzair","khilji");
+var c = a.reverse()
+document.write(c + "<br>")
+
+var a = new Array("fayyaz","ayyaz","uzair","khilji");
+ a.push("fayyaz is a good boy in the boys");
+document.write(a + "<br>")
+
+var a = new Array("fayyaz","ayyaz","uzair","khilji");
+a.reverse();
+a.push("ali asger" , "shanawaz");
+a.sort();
+
+var string1 = a.join();
+document.write(string1)
+document.write(typeof string1)
