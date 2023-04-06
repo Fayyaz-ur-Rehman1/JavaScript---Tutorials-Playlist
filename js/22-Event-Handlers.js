33cec4af880f28f13eb337e75d839f9c94345a02
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-04-06 10:28:59 +0530

    Event-Handlers

---
+++ js/22-Event-Handlers.js
@@ -0,0 +1,15 @@
+function btn(){
+    alert("Hello")
+}
+
+function onebtn(){
+    alert("Two-Time")
+}
+
+function secondbtn(){
+    alert("Three Time")
+}
+
+function thirdbtn(){
+    alert("Four-Time")
+}
