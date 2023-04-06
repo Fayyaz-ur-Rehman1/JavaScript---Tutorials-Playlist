5f817b890a8b091c5ce2b1efdfce78351490742e
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-04-06 10:27:10 +0530

    While-Loop

---
+++ js/20-While-Loop.js
@@ -0,0 +1,33 @@
+var x = 1 ;
+
+while(x < 10){
+    document.write(x + "my name is fayyazkhilji" + "<br>")
+    x++
+}
+
+
+document.write("<br>")
+
+var z = 5;
+
+while(z <= 50){
+    document.write(z + "<br>")
+    z=z+5
+}
+
+document.write("<br>")
+
+
+var y =  0;
+
+while(y <= 10){
+    document.write( y + ")  " + "HELLO BROTHER" + "<br>")
+    y++
+}
+
+var a = 10;
+
+while(a >= 1){
+    document.write(a + "fayyazurrehman" + "<br>");
+    a--
+}
